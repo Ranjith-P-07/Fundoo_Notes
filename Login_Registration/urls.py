6ea4fe3f416a0a81d148eb660482ce8fd569cf26
Ranjith P <Ranjithroy704@gmail.com>
2020-12-29 00:00:06 +0530

    "Add: Created url path for resetpassword"

--- Login_Registration/urls.py
+++ Login_Registration/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('forgotpassword/', views.Forgotpassword.as_view(), name="forgotpassword"),
+    path('reset_password/<surl>/', views.reset_password, name="reset_password"),
+    path('resetpassword/<user_reset>/', views.ResetPassword.as_view(), name="resetpassword"),
 ]
