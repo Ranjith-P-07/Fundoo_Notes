788bda19c9bf77cff4131f1082ac732482f67d68
Ranjith P <Ranjithroy704@gmail.com>
2020-12-31 00:16:34 +0530

    "Add: Note app is created and configured it in Installed apps"

--- Fundoo/settings.py
+++ Fundoo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Login_Registration.apps.LoginRegistrationConfig',
+    'Note',
 
     'rest_framework',
     'django_short_url',
