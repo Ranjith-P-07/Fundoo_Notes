8de5ea1ed12b89d6689fc54eba3cb94d439e7234
Ranjith P <Ranjithroy704@gmail.com>
2020-12-30 13:10:21 +0530

    "Add: Profile class is added for profile editing"

--- Login_Registration/models.py
+++ Login_Registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Registration(models.Model):
@@ -10,3 +12,10 @@ class Registration(models.Model):
 
     def __str__(self):
         return self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='defaultimage.png', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
