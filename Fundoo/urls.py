db90b06526cc93c9618f91b2ef745424d8f3964a
Ranjith P <Ranjithroy704@gmail.com>
2020-12-30 19:49:08 +0530

    "Add: added urlpatterns for Media_root so that it should show the profile image  stored in media folder"

--- Fundoo/urls.py
+++ Fundoo/urls.py
@@ -41,3 +41,9 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
