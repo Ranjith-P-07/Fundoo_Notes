f95ec0a5136e1fd8b23428a5a820f82793d5064a
Ranjith P <Ranjithroy704@gmail.com>
2021-01-02 02:16:25 +0530

    "Add: Logger is added "

--- Note/views.py
+++ Note/views.py
@@ -8,12 +8,21 @@ from django.contrib.auth.decorators import login_required
 
 from rest_framework import generics
 
+import logging
+from Fundoo.settings import file_handler
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+
+
 
 
 @method_decorator(login_required(login_url='/auth/login/'), name='dispatch')
 class ListNoteView(generics.ListAPIView):
     serializer_class = NotesSerializer
     queryset = Notes.objects.all()
+    logger.info("Notes listed succesfully..!!")
 
 
 
@@ -23,9 +32,10 @@ class NoteCreateView(GenericAPIView):
     queryset = Notes.objects.all()
     def get(self, request):
         user = request.user
-        print(user)
+        # print(user)
         notes = Notes.objects.filter(user_id = user.id, is_archive=False)
         serializer = NotesSerializer(notes, many=True)
+        logger.info("Particular Note is obtained, from get()")
         return Response(serializer.data, status=200)
 
 
@@ -35,7 +45,9 @@ class NoteCreateView(GenericAPIView):
         serializer = NotesSerializer(data=data, partial=True)
         if serializer.is_valid():
             serializer.save(user_id=user.id)
+            logger.info("New Note is created.")
             return Response(serializer.data, status=201)
+        logger.error("Something went wrong whlie creating Note, from post()")
         return Response(serializer.data, status=400)
 
 
@@ -52,6 +64,7 @@ class NoteUpdateView(GenericAPIView):
             print(queryset)
             return get_object_or_404(queryset, id=id)
         except Notes.DOesNotExit:
+            logger.error("id not present, from get_object()")
             return Response({'details': 'Id not present'})
 
     def get(self, request, id):
@@ -59,8 +72,10 @@ class NoteUpdateView(GenericAPIView):
             user = request.user
             note = self.get_object(request, id)
             serializer = NotesSerializer(note)
+            logger.info("got Note successfully, from get()")
             return Response(serializer.data, status=200)
         except Exception as e:
+            logger.error("something went wrong while getting Note, Enter the right id, from get()")
             return Response(e)
 
     def put(self, request, id):
@@ -71,9 +86,12 @@ class NoteUpdateView(GenericAPIView):
             serializer = NotesSerializer(instance, data=data)
             if serializer.is_valid():
                 note_update = serializer.save(user_id=user.id)
+                logger.info("Note updated succesfully, from put()")
                 return Response({'details': 'Note updated succesfully'})
+            logger.error("Note is not Updated something went wrong, from put()")
             return Response({'deatils': 'Note is not Updated..!!!'})
         except Exception as e:
+            logger.error("Something went wrong")
             return Response(e)
 
     def delete(self, request, id):
@@ -82,13 +100,15 @@ class NoteUpdateView(GenericAPIView):
             data = request.data
             instance = self.get_object(request, id)
             instance.delete()
+            logger.info("Note is Deleted Succesfully, from delete()")
             return Response({'details': 'Note is Deleted'})
         except:
+            logger.error("Something went wrong")
             return Response({'details': 'Note is not deleted..'})
 
 
 
-
+@method_decorator(login_required(login_url='/auth/login/'), name='dispatch')
 class LabelCreateView(GenericAPIView):
     serializer_class = LabelSerializer
     queryset = Label.objects.all()
@@ -98,8 +118,10 @@ class LabelCreateView(GenericAPIView):
             user = request.user
             labels = Label.objects.filter(user_id=user.id)
             serializer = LabelSerializer(labels, many=True)
+            logger.info("Got the Labels, from get()")
             return Response(serializer.data, status=200)
         except Exception as e:
+            logger.error("Something went wrong, from get()")
             return Response(e)
 
     def post(self, request):
@@ -107,10 +129,16 @@ class LabelCreateView(GenericAPIView):
         try:
             label=request.data['labelname']
             if label == "":
+                logger.error("labelname should be blank, from post()")
                 return Response({'details': 'labelname should be blank'})
             if Label.objects.filter(user_id=user.id, labelname=label).exists():
+                logger.error("label already exists, from post()")
                 return Response({'details': 'label already exists'})
             create_label = Label.objects.create(labelname=label, user_id=user.id)
+            logger.info("New Label is created.")
             return Response({'details': 'new label created'})
         except Exception as e:
+            logger.error("Something went wrong")
             return Response(e)
+
+ 
