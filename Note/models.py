cc39278471c10ac009dfcd73c69b5c823a25655a
Ranjith P <Ranjithroy704@gmail.com>
2021-01-02 01:10:39 +0530

    "Add: Created Label model "

--- Note/models.py
+++ Note/models.py
@@ -4,6 +4,15 @@ from datetime import datetime
 
 from colorful.fields import RGBColorField
 
+class Label(models.Model):
+    labelname = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.labelname
+
+
+
 class Notes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
@@ -13,6 +22,7 @@ class Notes(models.Model):
     is_pinned = models.BooleanField(default=False)
     date = models.DateTimeField(default=datetime.now, blank=True)
     color = RGBColorField(colors=['#FF0000', '#00FF00', '#0000FF'], blank=True, null=True)
+    label = models.ManyToManyField(Label, blank=True)
 
     def __str__(self):
         return str(self.user)
