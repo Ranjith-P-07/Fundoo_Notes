858b43e5ed6a166c35f7fc2a20a791480b727178
Ranjith P <Ranjithroy704@gmail.com>
2021-01-02 11:04:02 +0530

    "Add: labelupdate path is added"

--- Note/urls.py
+++ Note/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import NoteCreateView
+from .views import NoteCreateView, LabelCreateView, LabelUpdateView
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('list/', views.ListNoteView.as_view(), name='note-list'),
     path('noteupdate/<int:id>/', views.NoteUpdateView.as_view(), name='note-update'),
     path('label/', views.LabelCreateView.as_view(), name='create-label'),
+    path('labelupdate/<int:id>/', views.LabelUpdateView.as_view(), name='label-update'),
 ]
