2520b635bff0f9f4e48f24e3cc2d652308b22f15
Ranjith P <Ranjithroy704@gmail.com>
2021-01-14 22:54:00 +0530

    "Add: search-note url is added for searchbox API"

--- Note/urls.py
+++ Note/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import NoteCreateView, LabelCreateView, LabelUpdateView
+from .views import NoteCreateView, LabelCreateView, LabelUpdateView, TrashNoteAPI, ArchiveNoteView
 from . import views
 
 urlpatterns = [
     path('note/', views.NoteCreateView.as_view(), name='create_note'),
     path('list/', views.ListNoteView.as_view(), name='note-list'),
     path('noteupdate/<int:id>/', views.NoteUpdateView.as_view(), name='note-update'),
-    path('label/', views.LabelCreateView.as_view(), name='create-label'),
-    path('labelupdate/<int:id>/', views.LabelUpdateView.as_view(), name='label-update'),
+    path('label/', views.LabelCreateView.as_view(), name='create_label'),
+    path('labelupdate/<int:id>/', views.LabelUpdateView.as_view(), name='label_update'),
+    path('trash-note/', views.TrashNoteAPI.as_view(), name='Trash_note'),
+    path('Archive-note/', views.ArchiveNoteView.as_view(), name='Archive_note'),
+    path('Search-note/', views.SearchBoxView.as_view(), name='Search_note'),
 ]
