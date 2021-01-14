23e65e777e91a39fdfaf134a726c7b98d72cd0e4
Ranjith P <Ranjithroy704@gmail.com>
2021-01-15 02:05:11 +0530

    "Add: collaborator url path is added"

--- Note/urls.py
+++ Note/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from .views import NoteCreateView, LabelCreateView, LabelUpdateView, TrashNoteAPI, ArchiveNoteView
+from .views import NoteCreateView, LabelCreateView, LabelUpdateView, TrashNoteAPI, ArchiveNoteView, CollaboratorAPIView
 from . import views
 
 urlpatterns = [
-    path('note/', views.NoteCreateView.as_view(), name='create_note'),
+    path('archive-note/', views.ArchiveNoteView.as_view(), name='Archive_note'),
+    path('collabrator/', views.CollaboratorAPIView.as_view(), name='collaborator_note'),
     path('list/', views.ListNoteView.as_view(), name='note-list'),
-    path('noteupdate/<int:id>/', views.NoteUpdateView.as_view(), name='note-update'),
     path('label/', views.LabelCreateView.as_view(), name='create_label'),
     path('labelupdate/<int:id>/', views.LabelUpdateView.as_view(), name='label_update'),
+    path('note/', views.NoteCreateView.as_view(), name='create_note'),
+    path('noteupdate/<int:id>/', views.NoteUpdateView.as_view(), name='note-update'),
+    path('search-note/', views.SearchBoxView.as_view(), name='Search_note'),
     path('trash-note/', views.TrashNoteAPI.as_view(), name='Trash_note'),
-    path('Archive-note/', views.ArchiveNoteView.as_view(), name='Archive_note'),
-    path('Search-note/', views.SearchBoxView.as_view(), name='Search_note'),
 ]
