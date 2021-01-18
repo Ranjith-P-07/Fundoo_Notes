e12664e60d9e23ee30975527f0f3b593c5cbe70e
Ranjith P <Ranjithroy704@gmail.com>
2021-01-18 23:58:15 +0530

    "Add: Reminder-note url is added"

--- Note/urls.py
+++ Note/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('label/', views.LabelCreateView.as_view(), name='create_label'),
     path('labelupdate/<int:id>/', views.LabelUpdateView.as_view(), name='label_update'),
     path('note/', views.NoteCreateView.as_view(), name='create_note'),
-    path('noteupdate/<int:id>/', views.NoteUpdateView.as_view(), name='note-update'),
+    path('noteupdate/<int:id>/', views.NoteUpdateView.as_view(), name='note_update'),
+    path('reminder-note/<int:id>/', views.ReminderAPIView.as_view(), name='note_reminder'),
     path('search-note/', views.SearchBoxView.as_view(), name='Search_note'),
     path('trash-note/', views.TrashNoteAPI.as_view(), name='Trash_note'),
 ]
