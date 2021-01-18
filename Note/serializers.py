e277fa4d88064a8d7cbaf3877fc91e290f931f65
Ranjith P <Ranjithroy704@gmail.com>
2021-01-19 00:00:35 +0530

    "Add: Reminder serializer is added"

--- Note/serializers.py
+++ Note/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
 from .models import Notes, Label
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
     
 class NotesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
         fields = ['title', 'note', 'is_archive', 'is_trashed', 'is_pinned', 'color', 'label', 'collabrator']
+        extra_kwargs = {'label': {'read_only': True}, 'collabrator': {'read_only': True}}
 
 class LabelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Label
         fields = ['labelname']
+
+class ReminderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = ['reminder']
+    def validate_reminder(self, time):
+        if time.replace(tzinfo=None) - datetime.now() < timedelta(seconds=0):
+            raise serializers.ValidationError("Invaild reminder, Check your reminder once again..!!")
+        return time
