ba7d4cab6935942735288ecef1af6aeb5bf0d07b
Ranjith P <Ranjithroy704@gmail.com>
2021-01-18 23:58:47 +0530

    "Add: Reminder task is added"

--- Note/tasks.py
+++ Note/tasks.py
@@ -9,6 +9,17 @@ import time
 def delete_trashed_note():
     notes = Notes.objects.filter(is_trashed=True)
     for note in notes:
-        if datetime.now() - note.trashed_time.replace(tzinfo=None) > timedelta(minutes=1):
+        if datetime.now() - note.trashed_time.replace(tzinfo=None) > timedelta(days=7):
             note.delete()
-    return "Trashed deleted"
+    return "Trashed Note is deleted"
+
+@shared_task()
+def note_reminder():
+    notes = Notes.objects.filter(is_trashed=False).exclude(reminder=None)
+    print(datetime.now())
+    for note in notes:
+        if note.reminder.replace(tzinfo=None) - datetime.now() <= timedelta(minutes=1):
+            print(note.reminder)
+            note.reminder = None
+            note.save()
+            return "Reminder has been Triggered..!!!"
