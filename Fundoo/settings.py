594eadcc4be2111aa1d9b0263174f351191d361b
Ranjith P <Ranjithroy704@gmail.com>
2021-01-22 16:11:30 +0530

    "Add: Configured DEBUG to False and Allowed Host to all "

--- Fundoo/settings.py
+++ Fundoo/settings.py
@@ -34,9 +34,9 @@ file_handler.setFormatter(formatter)
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
